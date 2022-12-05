25f2ecfb174fc0f66ca2eed2b087d76b6ef75688
Islam <islam.bedretdinov@yandex.ru>
2022-12-05 23:23:47 +0300

    Курсовая работа

---
+++ src/Employee.java
@@ -0,0 +1,90 @@
+public class Employee {
+    private String namePerson;
+    private int division;
+    private int salary;
+
+    static int id = 0;
+
+
+    public Employee(String namePerson, int division, int salary) {
+        this.namePerson = namePerson;
+        this.division = division;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNamePerson() {
+        return namePerson;
+    }
+
+    public int getDivision() {
+        return division;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDivision(int division) {
+        this.division = division;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО: " + namePerson + " Отдел: " + division + " Зарплата: " + salary;
+    }
+
+    public static void listEmployee(Employee[] employees) {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public static void listSalary(Employee[] employees) {
+        int sum = 0;
+        int averageSum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            sum = sum + employee.salary;
+        }
+        System.out.println("Сумма затрат на зарплаты = " + sum);
+        averageSum = sum / 10;
+        System.out.println("Средняя зарплата = " + averageSum);
+    }
+
+    public static void listSalaryMin(Employee[] employees) {
+        int min = 50000;
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            if (min > employee.salary){
+                min = employee.salary;
+            id=i;
+            }
+        }
+        System.out.println("Сотрудник с минимальной зарплатой: " +employees[id]);
+    }
+    public static void listSalaryMax(Employee[] employees) {
+        int max = 0;
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            if (max < employee.salary){
+                max = employee.salary;
+            id=i;
+            }
+        }
+        System.out.println("Сотрудник с максимальной зарплатой: " +employees[id]);
+    }
+    public static void name(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            Employee employee = employees[i];
+            System.out.println("ФИО сотрудника: " + employee.namePerson);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] human = new Employee[10];
+        human[0] = new Employee("Иванов Иван Иванович", 1, 35000);
+        human[1] = new Employee("Иванов Сергей Андреевич", 1, 33000);
+        human[2] = new Employee("Алексеев Николай Андреевич", 2, 30000);
+        human[3] = new Employee("Федоров Федор Алексеевич", 2, 37000);
+        human[4] = new Employee("Андреев Андрей Александрович", 3, 30500);
+        human[5] = new Employee("Андреев Владислав Александрович", 3, 30500);
+        human[6] = new Employee("Еремин Антон Михайлович", 4, 40000);
+        human[7] = new Employee("Егоров Петр Евгеньевич", 4, 45000);
+        human[8] = new Employee("Алексеев Александр Витальевич", 5, 39000);
+        human[9] = new Employee("Степанов Максим Петрович", 5, 28000);
+        Employee.listEmployee(human);
+        Employee.listSalary(human);
+        Employee.listSalaryMin(human);
+        Employee.listSalaryMax(human);
+        Employee.name(human);
     }
 }
